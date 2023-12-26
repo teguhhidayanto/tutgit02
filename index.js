e08f4d829862309ffc7b1e95a5b5ba52d09ab71b
teguhhidayanto <learn.teguh@gmail.com>
2023-12-27 00:53:45 +0700

    fix conflict 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Selamat datang di program belajar git+github");
 
 console.log("menambahkan fitur search");
-=======
+
 console.log("menambahkan id pada label pencarian2");
->>>>>>> feat/searching
+
