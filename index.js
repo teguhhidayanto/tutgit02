274b69b27dc218add6a2f41e49cae1e35e250f6b
teguhhidayanto <learn.teguh@gmail.com>
2023-12-27 00:46:57 +0700

    feat/filter: menambahkan filter2

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("Selamat datang di program belajar git+github");
 console.log("menambahkan fitur search");
 
 console.log("menambahkan fitur filter");
+console.log("menambahkan fitur filter2");
