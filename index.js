2a5487f26174e513f3d52bd866a6a83836228615
teguhhidayanto <learn.teguh@gmail.com>
2023-12-27 00:55:39 +0700

    feat/searching: menambahkan name pada label pencarian

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log("Selamat datang di program belajar git+github");
 console.log("menambahkan fitur search");
 
 console.log("menambahkan id pada label pencarian2");
+
+console.log("menambahkan name pada label pencarian 1 dan 2");
